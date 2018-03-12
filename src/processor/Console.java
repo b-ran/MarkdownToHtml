881fc6ed6a6b64fac788858fe0abeec082b0652b
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-03-13 02:18:06 +1300

    Add temporary input system

--- src/processor/Console.java
+++ src/processor/Console.java
@@ -1,8 +1,20 @@
 package processor;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Console {
 
     public static void main(String[] args) {
-	// write your code here
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter File Path To Convert: ");
+//        String in = scanner.nextLine();
+//        System.out.println(in);
+        Interpreter interpreter = new Interpreter("test.txt");
+        interpreter.convert();
+//        StringBuilder stringBuilder = new StringBuilder(interpreter.convert());
+
+
+
     }
 }
