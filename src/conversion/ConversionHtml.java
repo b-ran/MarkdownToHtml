380e35d340f86faf23a862df6aa5b8cd5c21509e
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-03-14 20:54:44 +1300

    Start html conversion

--- src/conversion/ConversionHtml.java
+++ src/conversion/ConversionHtml.java
@@ -1,4 +1,29 @@
 package conversion;
 
-public class ConversionHtml {
+
+import markdownfeatures.*;
+import java.lang.*;
+
+public class ConversionHtml implements ConversionVisitor {
+
+
+    @Override
+    public String translate(Heading heading, String input) {
+        return "<h" + heading.getLevel() + "> " + input + " " + "</h" + heading.getLevel() + ">";
+    }
+
+    @Override
+    public String translate(Italic italic, String input) {
+        return null;
+    }
+
+    @Override
+    public String translate(Bold bold, String input) {
+        return null;
+    }
+
+    @Override
+    public String translate(Paragraph paragraph, String input) {
+        return null;
+    }
 }
