220c2e0e8942342e37bc39bf1724cfc90c4482c7
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-03-16 01:38:08 +1300

    Add italic & bold conversion with separation

--- src/conversion/ConversionHtml.java
+++ src/conversion/ConversionHtml.java
@@ -2,6 +2,8 @@ package conversion;
 
 
 import markdownfeatures.*;
+import markdownfeatures.separation.Separation;
+
 import java.lang.*;
 
 public class ConversionHtml implements ConversionVisitor {
@@ -18,15 +20,18 @@ public class ConversionHtml implements ConversionVisitor {
 
     @Override
     public StringBuilder translate(Italic italic, StringBuilder out) {
-        String inner = "<em>" + italic.getInput() + "</em>";
+        String inner = getText(italic, "<em>", "</em>");
         StringBuilder output = combine(inner, out);
-        nextLocation += inner.length();
+        nextLocation += inner.length()+1;
         return output;
     }
 
     @Override
     public StringBuilder translate(Bold bold, StringBuilder out) {
-        return null;
+        String inner = getText(bold, "<strong>", "</strong>");
+        StringBuilder output = combine(inner, out);
+        nextLocation += inner.length()+1;
+        return output;
     }
 
     @Override
@@ -38,11 +43,26 @@ public class ConversionHtml implements ConversionVisitor {
     public StringBuilder translate(Word word, StringBuilder out) {
         String inner = word.getInput();
         StringBuilder output = combine(inner, out);
-        nextLocation += inner.length();
+        nextLocation += inner.length()+1;
         return output;
     }
 
+    private String getText(Feature feature, String startTag, String exitTag) {
+        String inner = "";
+        Separation separation = feature.getSeparation();
+        if (separation.isSeparation()) {
+            if (separation.left()) {
+                inner = startTag + feature.getInput();
+            } else if (separation.right()) {
+                inner =  feature.getInput() + exitTag;
+            }
+        }
+        else inner = startTag + feature.getInput() + exitTag;
+        return inner;
+    }
+
     private StringBuilder combine (String inner, StringBuilder out) {
+        inner += " ";
         out.insert(nextLocation, inner);
         return out;
     }
