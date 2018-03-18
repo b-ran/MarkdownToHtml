d96529e7b6e56c77f1ef21afccdca0a4954fd78e
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-03-18 22:34:17 +1300

    Add null tests

--- tests/tests/NullTest.java
+++ tests/tests/NullTest.java
@@ -1,4 +1,43 @@
 package tests;
 
+import markdownfeatures.NullFeature;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class NullTest {
+
+    @Test
+    public void test01_string_detection_invalid() throws Exception {
+        assertFalse(new NullFeature().detect("", ""));
+    }
+
+    @Test
+    public void test02_string_detection_invalid() throws Exception {
+        assertFalse(new NullFeature().detect("shortcuts", "shortcuts around red"));
+    }
+
+    @Test
+    public void test03_string_detection_invalid() throws Exception {
+        assertFalse(new NullFeature().detect("$%\n", "$&\n asdas"));
+    }
+
+    @Test
+    public void test04_input_unchangeable() throws Exception {
+        NullFeature paragraph = new NullFeature();
+        paragraph.setInput("&HSHD JS&^666");
+        assertNotEquals(paragraph.getInput(), "&HSHD JS&^666");
+    }
+
+    @Test
+    public void test05_convertible() throws Exception {
+        assertFalse(new NullFeature().convertible());
+    }
+
+    @Test
+    public void test06_separation() throws Exception {
+        NullFeature paragraph = new NullFeature();
+        assertFalse(paragraph.detect("\njdjidfs847", "\njdjidfs847 jjds&&$****   "));
+        assertFalse(paragraph.getSeparation().isSeparation());
+    }
 }
