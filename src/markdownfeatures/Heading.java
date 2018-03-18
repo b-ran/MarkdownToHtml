4903689690b79d4ffc38cae6343d88e239aedde9
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-03-18 17:48:30 +1300

    Fixed heading bug not starting at line & add support for 3 space gap

--- src/markdownfeatures/Heading.java
+++ src/markdownfeatures/Heading.java
@@ -4,6 +4,8 @@ import conversion.ConversionVisitor;
 import markdownfeatures.separation.SeparateNull;
 import markdownfeatures.separation.Separation;
 
+import java.util.Scanner;
+
 public class Heading implements Feature {
 
     private static final int LIMIT = 6;
@@ -13,7 +15,14 @@ public class Heading implements Feature {
     @Override
     public boolean detect(String next, String line) {
         String in = next.trim();
-        if (in.length() == 0) return false;
+
+        if (in.length() == 0 || line.length() == 0) return false;
+        if (!new Scanner(line).next().equals(in)) return false;
+
+        if (line.length() > 3) {
+            if (line.charAt(3) == ' ') return false;
+        }
+
         for (char letter : in.toCharArray()) {
             level++;
             if (letter != '#') return false;
@@ -56,65 +65,3 @@ public class Heading implements Feature {
     }
 }
 
-
-//package markdownfeatures;
-//
-//import conversion.ConversionVisitor;
-//import markdownfeatures.separation.SeparateNull;
-//import markdownfeatures.separation.Separation;
-//
-//import java.util.Scanner;
-//
-//public class Heading implements Feature {
-//
-//    private static final int LIMIT = 6;
-//    private int level = 0;
-//    private String input = "";
-//
-//    @Override
-//    public boolean detect(String next, String line) {
-//        String in = line.trim();
-//        if (in.length() == 0) return false;
-//        in = new Scanner(line).next();
-//        for (char letter : in.toCharArray()) {
-//            if (letter != '#') return false;
-//            if (level > LIMIT) return false;
-//            level++;
-//        }
-//        return true;
-//    }
-//
-//
-//    public int getLevel() {
-//        return level;
-//    }
-//
-//    @Override
-//    public void setInput(String input) {
-//        this.input = format(input).trim();
-//    }
-//
-//    public String format(String input) {
-//        return input.substring(level, input.length());
-//    }
-//
-//    @Override
-//    public String getInput() {
-//        return input;
-//    }
-//
-//    @Override
-//    public StringBuilder convert(ConversionVisitor conversionVisitor, StringBuilder out) {
-//       return conversionVisitor.translate(this, out);
-//    }
-//
-//    @Override
-//    public Separation getSeparation() {
-//        return new SeparateNull();
-//    }
-//
-//    @Override
-//    public boolean convertible() {
-//        return true;
-//    }
-//}
