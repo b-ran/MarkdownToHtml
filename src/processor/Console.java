56a0146c7a7388e9ff7d5fa117ca237f3931704f
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-03-18 17:16:14 +1300

    Fixed input system & add file writing

--- src/processor/Console.java
+++ src/processor/Console.java
@@ -1,20 +1,32 @@
 package processor;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Console {
 
+    private static final String fileFormat = ".txt";
+
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter File Path To Convert: ");
-//        String in = scanner.nextLine();
-//        System.out.println(in);
-        Interpreter interpreter = new Interpreter("test.txt");
-        interpreter.convert();
-//        StringBuilder stringBuilder = new StringBuilder(interpreter.convert());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter File Path To Convert: ");
+        String in = scanner.nextLine();
 
+        Interpreter interpreter = new Interpreter(in);
+        write(in, interpreter.convert());
+    }
 
+    private static void write(String in, StringBuilder stringBuilder) {
+        File fileOut = new File(in.substring(0,in.length()-fileFormat.length()) + "-convert" + ".html");
 
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileOut))) {
+            System.out.println(stringBuilder.toString());
+            bufferedWriter.append(stringBuilder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
