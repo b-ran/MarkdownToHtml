f8b65e7152147800bd721eeacc34a1b9dc2c6110
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-03-19 20:25:28 +1300

    Added inline feature w/detection

---
+++ src/markdownfeatures/Inline.java
@@ -0,0 +1,65 @@
+package markdownfeatures;
+
+import conversion.ConversionVisitor;
+import markdownfeatures.separation.SeparateNull;
+import markdownfeatures.separation.SeparateTags;
+import markdownfeatures.separation.Separation;
+
+public class Inline implements Feature {
+
+    private String input = "";
+    private final static String format = "`";
+    private Separation separation = new SeparateNull();
+
+    @Override
+    public boolean detect(String next, String line) {
+        String in = next.trim();
+        if (in.length() == 0) return false;
+        if (separation.detect(next)) return true;
+
+        if (in.length() > 1) {
+            if (in.charAt(1) == format.charAt(0) || !in.startsWith(format)) return false;
+        }
+        if(SeparateTags.detectSeparation(in, line, format)) {
+            separation = new SeparateTags(format);
+            return true;
+        }
+        if (in.length() > 1) {
+            if (in.charAt(in.length()-2) == format.charAt(0) || !in.endsWith(format)) return false;
+        }
+        return true;
+    }
+
+    public Separation getSeparation() {
+        return separation;
+    }
+
+    @Override
+    public boolean convertible() {
+        return true;
+    }
+
+    @Override
+    public void setInput(String input) {
+        this.input = input;
+        format();
+    }
+
+    private void format() {
+        if (separation.isSeparation()) input = separation.format(input);
+        else {
+            input = input.substring(format.length());
+            input = input.substring(0, input.length()-format.length());
+        }
+    }
+
+    @Override
+    public String getInput() {
+        return input;
+    }
+
+    @Override
+    public StringBuilder convert(ConversionVisitor conversionVisitor, StringBuilder out) {
+        return conversionVisitor.translate(this, out);
+    }
+}
