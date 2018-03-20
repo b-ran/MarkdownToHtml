21672d984c042fe61b93d1f331a0e03a6d8ea9f2
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-03-20 23:39:53 +1300

    Add tests for numbered list

---
+++ tests/tests/NumberedListTest.java
@@ -0,0 +1,69 @@
+package tests;
+
+import markdownfeatures.NumberedList;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NumberedListTest {
+
+    @Test
+    public void test01_string_detection_valid() throws Exception {
+        assertTrue(new NumberedList().detect("1.", "1. item one"));
+    }
+
+    @Test
+    public void test02_string_detection_valid_sublist() throws Exception {
+        NumberedList numberedList = new NumberedList();
+        assertTrue(numberedList.detect("8.", "8. item eight"));
+        assertFalse(numberedList.isSublist());
+        assertTrue(numberedList.detect("-", "- sublist"));
+        assertTrue(numberedList.isSublist());
+        assertTrue(numberedList.detect("-", "- sublist"));
+        assertTrue(numberedList.isSublist());
+        assertTrue(numberedList.detect("9.", "9. item nine"));
+        assertFalse(numberedList.isSublist());
+    }
+
+    @Test
+    public void test03_string_detection_invalid() throws Exception {
+        assertFalse(new NumberedList().detect("2.asd", "*2.asd not a list item"));
+        assertFalse(new NumberedList().detect("#,hello", "#,hello"));
+    }
+
+    @Test
+    public void test04_spacing_invalid() throws Exception {
+        NumberedList numberedList = new NumberedList();
+        assertFalse(numberedList.detect("     1.", "     1. item one"));
+        assertTrue(numberedList.detect("1.", "1. item one"));
+        assertFalse(numberedList.detect("      -", "       - sublist"));
+    }
+
+    @Test
+    public void test05_spacing_valid() throws Exception {
+        NumberedList numberedList = new NumberedList();
+        assertTrue(numberedList.detect("    1.", "    1. item one"));
+        assertTrue(numberedList.detect("1.", "1. item one"));
+        assertTrue(numberedList.detect("     -", "      - sublist"));
+    }
+
+    @Test
+    public void test06_list_then_sublist() throws Exception {
+        NumberedList numberedList = new NumberedList();
+        assertFalse(numberedList.detect("-", "   - sublist"));
+        assertTrue(numberedList.detect("1.", "1. item one"));
+        assertTrue(numberedList.detect("-", "   - sublist"));
+    }
+
+    @Test
+    public void test07_convertible() throws Exception {
+        assertTrue(new NumberedList().convertible());
+    }
+
+    @Test
+    public void test08_separation_creation() throws Exception {
+        NumberedList numberedList = new NumberedList();
+        assertTrue(numberedList.detect("1.", "1. item one"));
+        assertTrue(numberedList.getSeparation().isSeparation());
+    }
+}
