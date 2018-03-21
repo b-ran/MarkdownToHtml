496d9ff9dc52b7859b5867010cafae993524cd49
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-03-22 01:07:24 +1300

    Add bulleted list decetion

--- src/markdownfeatures/BulletedList.java
+++ src/markdownfeatures/BulletedList.java
@@ -6,7 +6,7 @@ import markdownfeatures.separation.Separation;
 public class BulletedList extends List{
 
     public BulletedList() {
-        super(4, 6, '*', '*');
+        super(2, 6, '*', '*');
     }
 
     @Override
@@ -16,16 +16,32 @@ public class BulletedList extends List{
 
     @Override
     boolean checkValidLineSpacing(String line) {
+        if (!line.startsWith(" ")) return true;
+        String in = line.trim();
+        if (in.charAt(0) == listFormat  && maxSpacesList >= getTotalStartingSpace(line)) {
+            return true;
+        }
+        Integer startWordIndex = getStartWordIndex(line);
+        if (startWordIndex >= oldStartWordIndex) {
+            return (oldStartWordIndex + maxSpacesSubList >= getTotalStartingSpace(line));
+        }
+
         return false;
     }
 
-    @Override
     boolean checkValidListFormat(String line) {
+        String in = line.trim();
+        if (in.length() > 2 && maxSpacesList >=  getTotalStartingSpace(line)) {
+            return in.charAt(0) == listFormat && in.charAt(1) == ' ';
+        }
         return false;
     }
 
-    @Override
     boolean checkValidSubListFormat(String line) {
+        String in = line.trim();
+        if (in.length() > 2 && separation.isSeparation()) {
+            return in.charAt(0) == sublistFormat && in.charAt(1) == ' ';
+        }
         return false;
     }
 }
