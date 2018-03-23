854ee09c5521992330a9ed007c7576ab406b8492
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-03-23 23:26:43 +1300

    Refactored html conversion to work with the new tag system & Add list translation

--- src/conversion/ConversionHtml.java
+++ src/conversion/ConversionHtml.java
@@ -1,15 +1,14 @@
 package conversion;
 
 
+import conversion.tag.*;
 import markdownfeatures.*;
 import markdownfeatures.separation.Separation;
 
 import java.lang.*;
+import java.util.ArrayList;
 
 public class ConversionHtml implements ConversionVisitor {
-
-    //TODO: fix code smell better system of defining tags
-    //TODO: move numberList translation to able to used for all types of lists
 
     private int nextLocation = 0;
     private boolean newParagraph = true;
@@ -20,36 +19,35 @@ public class ConversionHtml implements ConversionVisitor {
 
     @Override
     public StringBuilder translate(Heading heading, StringBuilder out) {
-        String startTag = "\n<h" + heading.getLevel() + ">";
-        String outTag = "</h" + heading.getLevel() + ">";
-        StringBuilder output = combine(startTag+outTag, out);
-        nextLocation += startTag.length();
+        HeadingTag headingTag = new HeadingTag(heading);
+        StringBuilder output = combineWithStringBuilder(headingTag.startHtmlTag+headingTag.endHtmlTag, out);
+        nextLocation += headingTag.nextHtmlLocation();
         return output;
     }
 
     @Override
     public StringBuilder translate(Italic italic, StringBuilder out) {
-        String inner = getText(italic, "<em>", "</em>");
-        StringBuilder output = combine(inner, out);
-        nextLocation += inner.length()+1;
+        String inner = chooseCurrentSeparationTag(italic, ItalicTag.startHtmlTag, ItalicTag.endHtmlTag);
+        StringBuilder output = combineWithStringBuilder(inner, out);
+        nextLocation += ItalicTag.nextHtmlLocation();
         return output;
     }
 
     @Override
     public StringBuilder translate(Bold bold, StringBuilder out) {
-        String inner = getText(bold, "<strong>", "</strong>");
-        StringBuilder output = combine(inner, out);
-        nextLocation += inner.length()+1;
+        String inner = chooseCurrentSeparationTag(bold, BoldTag.startHtmlTag, BoldTag.endHtmlTag);
+        StringBuilder output = combineWithStringBuilder(inner, out);
+        nextLocation += BoldTag.nextHtmlLocation();
         return output;
     }
 
     @Override
     public StringBuilder translate(Paragraph paragraph, StringBuilder out) {
         if (newParagraph) {
-            out.append("\n<p>\n");
+            out.append(ParagraphTag.startHtmlTag);
             newParagraph = !newParagraph;
         } else if (endParagraph) {
-            out.append("\n</p>\n<p>");
+            out.append(ParagraphTag.endHtmlTag);
         }
         endParagraph = false;
         nextLocation = out.length();
@@ -58,16 +56,16 @@ public class ConversionHtml implements ConversionVisitor {
 
     @Override
     public StringBuilder translate(Word word, StringBuilder out) {
-        String inner = word.getInput();
-        StringBuilder output = combine(inner, out);
-        nextLocation += inner.length()+1;
+        String text = word.getInput();
+        StringBuilder output = combineWithStringBuilder(text, out);
+        nextLocation += text.length()+1;
         return output;
     }
 
     @Override
     public StringBuilder translate(Separator separator, StringBuilder out) {
-        StringBuilder output = end(out);
-        output.append("\n<hr>");
+        StringBuilder output = endFile(out);
+        output.append(SeparatorTag.HtmlTag);
         nextLocation = output.length();
         translate(new Paragraph(), out);
         return output;
@@ -75,81 +73,86 @@ public class ConversionHtml implements ConversionVisitor {
 
     @Override
     public StringBuilder translate(Blockquote blockquote, StringBuilder out) {
-        String startTag = "\n<blockquote>";
-        String endTag = "</blockquote>";
-        out.append(startTag+endTag);
-        nextLocation += startTag.length();
+        out.append(BlockquoteTag.startHtmlTag+BlockquoteTag.endHtmlTag);
+        nextLocation += BlockquoteTag.nextHtmlLocation();
         return out;
     }
 
     @Override
     public StringBuilder translate(Inline inline, StringBuilder out) {
-        String inner = getText(inline, "<code>", "</code>");
-        StringBuilder output = combine(inner, out);
-        nextLocation += inner.length()+1;
+        String inner = chooseCurrentSeparationTag(inline, InlineTag.startHtmlTag, InlineTag.endHtmlTag);
+        StringBuilder output = combineWithStringBuilder(inner, out);
+        nextLocation += InlineTag.nextHtmlLocation();
         return output;
     }
 
     @Override
     public StringBuilder translate(Block block, StringBuilder out) {
-        String inner = getText(block, "<code>", "</code>");
+        String inner = chooseCurrentSeparationTag(block, BlockTag.startHtmlTag, BoldTag.endHtmlTag);
         out.append(inner);
-        nextLocation = out.length();
+        nextLocation = BoldTag.nextHtmlLocation();
         return out;
     }
 
     @Override
     public StringBuilder translate(NumberedList numberedList, StringBuilder out) {
-        Integer tagLength = 4;
-        if (startOfList) {
-            out.append("<ol>\n</ol>");
-            nextLocation+=5;
-            startOfList = false;
-        } else {
-            nextLocation = out.length()-5;
-        }
-        if (!numberedList.isSublist()) startOfSubList = true;
-
-
-        if (startOfSubList && numberedList.isSublist()) {
-            nextLocation -= 6;
-            startOfSubList = false;
-            out.insert(nextLocation,"\n<ul>\n</ul>\n");
-            nextLocation+=6;
-        }
-        out.insert(nextLocation,"<li></li>\n");
-        nextLocation+=4;
-        return out;
+        return convertList(numberedList, new NumberedListTag(), out);
     }
 
     @Override
     public StringBuilder translate(BulletedList bulletedList, StringBuilder out) {
-        return null;
+        return convertList(bulletedList, new BulletedListTag(), out);
     }
 
     @Override
-    public StringBuilder end(StringBuilder out) {
+    public StringBuilder startFile(StringBuilder out) {
+        return out;
+    }
+
+    @Override
+    public StringBuilder endFile(StringBuilder out) {
         if (!newParagraph) {
-            out.append("\n</p>");
+            out.append(ParagraphTag.endFileHtmlTag);
         }
         return out;
     }
 
-    private String getText(Feature feature, String startTag, String exitTag) {
+    private StringBuilder convertList(ListFeature list, ListTag listTag, StringBuilder out) {
+        if (startOfList) {
+            out.append(listTag.htmlTypeTag());
+            nextLocation = listTag.nextLocationHtmlTypeTag();
+            startOfList = false;
+        } else {
+            nextLocation = out.length() - listTag.nextLocationHtmlTypeTag();
+        }
+        if (!list.isSublist()) startOfSubList = true;
+
+        if (startOfSubList && list.isSublist()) {
+            nextLocation -= listTag.nextLocationHtmlNestedTag();
+            startOfSubList = false;
+            out.insert(nextLocation, listTag.htmlNestedTag());
+            nextLocation += listTag.nextLocationHtmlNestedTag();
+        }
+        out.insert(nextLocation, listTag.htmlItemTag());
+        nextLocation += listTag.nextLocationHtmlItemTag();
+        return out;
+    }
+
+    private String chooseCurrentSeparationTag(Feature feature, String startTag, String endTag) {
         String inner = "";
         Separation separation = feature.getSeparation();
         if (separation.isSeparation()) {
             if (separation.left()) {
                 inner = startTag + feature.getInput();
             } else if (separation.right()) {
-                inner =  feature.getInput() + exitTag;
+                inner =  feature.getInput() + endTag;
             }
         }
-        else inner = startTag + feature.getInput() + exitTag;
+        else inner = startTag + feature.getInput() + endTag;
         return inner;
     }
 
-    private StringBuilder combine (String inner, StringBuilder out) {
+    private StringBuilder combineWithStringBuilder(String inner, StringBuilder out) {
         endParagraph = true;
         inner += " ";
         out.insert(nextLocation, inner);
